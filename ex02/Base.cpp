f4d32c5627d2c968c49c231c320027768b1eeb27
MiguelLlamas <63542440+42MiguelLlamas@users.noreply.github.com>
2024-10-09 12:12:25 +0000

    ex02 in process

--- ex02/Base.cpp
+++ ex02/Base.cpp
@@ -1,12 +1,60 @@
-#include "Serializer.hpp"
+#include "Base.hpp"
 
-
-uintptr_t Serializer::serialize(Data* ptr)
-{
-    return reinterpret_cast<uintptr_t>(ptr);
+Base::~Base() {
+    std::cout << "Base destructor called" << std::endl;
 }
 
-Data* Serializer::deserialize(uintptr_t raw)
+Base* generate(void)
 {
-    return reinterpret_cast<Data*>(raw);
+    std::srand(std::time(0));
+    int randValue = std::rand() % 3;
+
+    switch (randValue)
+    {
+        case 0:
+            std::cout << "Generated A" << std::endl;
+            return new A();
+        case 1:
+            std::cout << "Generated B" << std::endl;
+            return new B();
+        case 2:
+            std::cout << "Generated C" << std::endl;
+            return new C();
+        default:
+            return NULL;
+    }
+}
+
+void identify(Base *p) {
+  if (dynamic_cast<A *>(p))
+    std::cout << "Class A" << std::endl;
+  else if (dynamic_cast<B *>(p))
+    std::cout << "Class B" << std::endl;
+  else if (dynamic_cast<C *>(p))
+    std::cout << "Class C" << std::endl;
+  else
+    std::cout << "Not Class A, B or C" << std::endl;
+}
+
+void identify(Base &p) {
+	Base temp;
+	
+  try {
+    temp = dynamic_cast<A &>(p);
+		(void) temp;
+    std::cout << "Class A" << std::endl;
+  } catch (std::exception &e) {
+  }
+  try {
+    temp = dynamic_cast<B &>(p);
+		(void) temp;
+    std::cout << "Class B" << std::endl;
+  } catch (std::exception &e) {
+  }
+  try {
+    temp = dynamic_cast<C &>(p);
+		(void) temp;
+    std::cout << "Class C" << std::endl;
+  } catch (std::exception &e) {
+  }
 }
--- ex02/main.cpp
+++ ex02/main.cpp
@@ -1,56 +1,5 @@
 #include "Base.hpp"
-#include "A.hpp"
-#include "B.hpp"
-#include "C.hpp"
-#include <iostream>
-#include <cstdlib>
-#include <ctime>
 
-Base* generate(void)
-{
-    srand(time(0)); 
-    int random = rand() % 3;
-
-    if (random == 0)
-        return new A();
-    else if (random == 1)
-        return new B();
-    else
-        return new C();
-}
-
-void identify(Base* p)
-{
-    if (dynamic_cast<A*>(p))
-        std::cout << "Type is A" << std::endl;
-    else if (dynamic_cast<B*>(p))
-        std::cout << "Type is B" << std::endl;
-    else if (dynamic_cast<C*>(p))
-        std::cout << "Type is C" << std::endl;
-    else
-        std::cout << "Unknown type" << std::endl;
-}
-
-void identify(Base& p)
-{
-    try {
-        A& a = dynamic_cast<A&>(p);
-        (void)a;
-        std::cout << "Type is A" << std::endl;
-    } catch (std::bad_cast&) {}
-
-    try {
-        B& b = dynamic_cast<B&>(p);
-        (void)b;
-        std::cout << "Type is B" << std::endl;
-    } catch (std::bad_cast&) {}
-
-    try {
-        C& c = dynamic_cast<C&>(p);
-        (void)c;
-        std::cout << "Type is C" << std::endl;
-    } catch (std::bad_cast&) {}
-}
 int main()
 {
     Base* basePtr = generate();
